9768b30a55f3e8aed6b58a0fea7143fa813bbd65
Escobarly Amaral <escobarlyamarall@gmail.com>
2026-06-05 09:59:24 -0300

    Novas páginas, script e css relacionadas aos semestres e adições do href nas âncoras do cabeçalho

---
+++ style/semestres.js
@@ -0,0 +1,48 @@
+// Controle de Turno
+
+document.addEventListener("DOMContentLoaded", () => {
+    const switchTurno = document.getElementById("switch-turno");
+    const pooPrereqCells = document.querySelectorAll(".poo-prereq-value");
+    const turnoAtualBadges = document.querySelectorAll(".turno-atual-value");
+    const colEadElements = document.querySelectorAll(".col-ead");
+
+    const turnoSalvo = localStorage.getItem("ads-turno") || "diurno";
+
+    if (switchTurno) {
+        switchTurno.checked = (turnoSalvo === "noturno");
+    }
+
+    aplicarTurno(turnoSalvo);
+
+    if (switchTurno) {
+        switchTurno.addEventListener("change", (e) => {
+            const novoTurno = e.target.checked ? "noturno" : "diurno";
+            localStorage.setItem("ads-turno", novoTurno);
+            aplicarTurno(novoTurno);
+        });
+    }
+
+    function aplicarTurno(turno) {
+        if (turno === "noturno") {
+            pooPrereqCells.forEach(cell => {
+                cell.innerHTML = `<a href="1-semestre.html" class="prereq-link" title="Lógica e Linguagem de Programação">LLPR</a>`;
+            });
+            turnoAtualBadges.forEach(badge => {
+                badge.innerHTML = `<i class="material-icons">nights_stay</i> Noturno`;
+            });
+            colEadElements.forEach(el => {
+                el.classList.remove("oculto");
+            });
+        } else {
+            pooPrereqCells.forEach(cell => {
+                cell.innerHTML = `<a href="2-semestre.html" class="prereq-link" title="Programação Estruturada e Estrutura de Dados">PEED</a>`;
+            });
+            turnoAtualBadges.forEach(badge => {
+                badge.innerHTML = `<i class="material-icons">wb_sunny</i> Matutino / Vespertino`;
+            });
+            colEadElements.forEach(el => {
+                el.classList.add("oculto");
+            });
+        }
+    }
+});
