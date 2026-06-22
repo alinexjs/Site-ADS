25a27b08ecfb57e746494bee3085fbe1df0205a9
alinexsj <agm.linne@gmail.com>
2026-06-22 09:56:24 -0300

    style(footer, contato, home): formatação do footer em todas as páginas. Algumas alterações em home e contato.

---
+++ javascript/colacao-grau.js
@@ -0,0 +1,8 @@
+    window.addEventListener('load', () => {
+        confetti({
+            particleCount: 200,
+            spread: 160,
+            startVelocity: 40,
+            colors: ['#ffffff', '#facc15', '#0f766e']
+        });
+    });
